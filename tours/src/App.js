0a63c095c315816e314d36af263c9534663bdd69
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-05-27 00:56:59 +0900

    react 연습

---
+++ tours/src/App.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+import "./App.css";
+
+const url = "https://course-api.com/react-tours-project";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => {
+      return tour.id !== id;
+    });
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(false);
+
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  // []의 의미: 이 웹앱이 실행되는 딱 한 번만 useEffect 안에 있는 내용이 실행됨
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No Tours Left</h2>
+          <button onClick={fetchTours}>Refresh</button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App;

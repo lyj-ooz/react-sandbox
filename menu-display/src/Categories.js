38587681cd44b3ab22bbdeee61c151cd4547a560
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-06-04 23:24:16 +0900

    menu display starter

---
+++ menu-display/src/Categories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div>
+      <p>i am category</p>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ menu-display/src/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      <p>i am menu!</p>
+    </div>
+  );
+};
+
+export default Menu;

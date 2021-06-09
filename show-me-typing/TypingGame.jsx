96b971a83e4b02893eba7bc172fb58e29300a923
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-06-09 23:53:59 +0900

    타이틀 수정

--- show-me-typing/TypingGame.jsx
+++ show-me-typing/TypingGame.jsx
@@ -5,7 +5,7 @@ import Ranking from "./components/Ranking";
 const TypingGame = () => {
   return (
     <div>
-      <h1>i am typinggame yayoo</h1>
+      <h1>SHOW ME YOUR TYPING!</h1>
       <Ranking />
       <Game />
     </div>

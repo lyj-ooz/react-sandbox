0a69a285d5298d25796dc89842a7c13f2a817931
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-06-10 00:01:52 +0900

    Game.jsx 마크업

--- show-me-typing/components/Game.jsx
+++ show-me-typing/components/Game.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Game = () => {
   return (
-    <div>
-      <div>10초</div>
-      <div>Apple</div>
+    <div className="game-container">
+      <div className="time">10</div>
+      <div className="word">Apple</div>
       <input type="text" />
       <button>Start Game</button>
+      <div className="score">
+        Score: <span>300</span>
+      </div>
     </div>
   );
 };

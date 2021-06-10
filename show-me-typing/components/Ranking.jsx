437041caf70fe82f278bfad2c68ce1b49e83aa60
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-06-10 17:33:31 +0900

    Ranking.jsx 마크업

--- show-me-typing/components/Ranking.jsx
+++ show-me-typing/components/Ranking.jsx
@@ -2,8 +2,23 @@ import React from "react";
 
 const Ranking = () => {
   return (
-    <div>
-      <p>i am ranking</p>
+    <div
+      className="ranking-container"
+      style={{ backgroundColor: "lightgreen" }}
+    >
+      <div className="ranking-title">RANKING</div>
+      <div className="ranking-list">
+        <div className="ranking-people">
+          <div className="ranking-person">noname</div>
+          <div className="ranking-person">yello123</div>
+          <div className="ranking-person">whale000</div>
+        </div>
+        <div className="ranking-scores">
+          <div className="ranking-score">100</div>
+          <div className="ranking-score">150</div>
+          <div className="ranking-score">783</div>
+        </div>
+      </div>
     </div>
   );
 };

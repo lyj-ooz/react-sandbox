e290be6d82746563a33cfd8ff7811e60a0f34b10
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-06-10 20:30:40 +0900

    Title 컴포넌트 마크업와 스타일링

---
+++ show-me-typing/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.css";
+
+const Title = () => {
+  return (
+    <header>
+      <h1>SHOW ME YOUR TYPING!</h1>
+    </header>
+  );
+};
+
+export default Title;

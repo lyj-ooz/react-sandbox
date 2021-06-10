c621f3f64f9090ba659772fab461ab4c31417b91
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-06-10 17:26:19 +0900

    className 수저

--- show-me-typing/components/Game.jsx
+++ show-me-typing/components/Game.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Game = () => {
   return (
-    <div className="game-container">
-      <div className="time">10</div>
-      <div className="word">Apple</div>
+    <div className="game-container" style={{ backgroundColor: "lightsalmon" }}>
+      <div className="game-time">10</div>
+      <div className="game-word">Apple</div>
       <input type="text" />
       <button>Start Game</button>
-      <div className="score">
+      <div className="game-score">
         Score: <span>300</span>
       </div>
     </div>

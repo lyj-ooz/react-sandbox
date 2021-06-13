920dda0b12f625cfe22fc3fa4cd5fa101294eb8f
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-06-13 14:38:11 +0900

    gitignore 파일 수정

---
+++ show-me-typing/backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const scoresRoute = require("./routes/scores");
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("mongo db connected!");
+  })
+  .catch((err) => console.log(err));
+
+app.use("/api/scores", scoresRoute);
+
+app.listen(8888, () => {
+  console.log("backend server is running...");
+});
